2c7b3bac52913f2c03ab1311e4381ee2a18aaa5d
fredmaggiowski <federicomaggi@me.com>
2015-05-26 12:09:26 +0200

    implemented chat fragment

---
+++ app/src/main/java/me/federicomaggi/suggestme/ChatFragment.java
@@ -0,0 +1,108 @@
+package me.federicomaggi.suggestme;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ChatFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ChatFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ChatFragment extends Fragment {
+
+    public static final String SOCIAL = "social";
+    public static final String GOODS  = "goods";
+
+    private static final String CATEGORY = "";
+
+    private String category;
+
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param category Parameter 1.
+     * @return A new instance of fragment ChatFragment.
+     */
+    public static ChatFragment newInstance(String category) {
+        ChatFragment fragment = new ChatFragment();
+        Bundle args = new Bundle();
+        args.putString(CATEGORY, category);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ChatFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            this.category = getArguments().getString(CATEGORY);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        TextView mTextView = (TextView) rootView.findViewById(R.id.test_textview);
+        mTextView.setText(this.category);
+
+        return rootView;
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
--- app/src/main/java/me/federicomaggi/suggestme/SceltaCategorie.java
+++ app/src/main/java/me/federicomaggi/suggestme/SceltaCategorie.java
@@ -7,27 +7,24 @@ import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
+import android.widget.ImageButton;
 
 
 public class SceltaCategorie extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks, LeMieDomande.OnFragmentInteractionListener {
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks,
+                   LeMieDomandeFragment.OnFragmentInteractionListener,
+                   LoginFragment.OnFragmentInteractionListener,
+                   AboutFragment.OnFragmentInteractionListener,
+                   ChatFragment.OnFragmentInteractionListener {
 
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
     private NavigationDrawerFragment mNavigationDrawerFragment;
 
-
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
     private CharSequence mTitle;
 
     @Override
@@ -39,44 +36,49 @@ public class SceltaCategorie extends ActionBarActivity
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
 
-        // Set up the drawer.
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, SceltaCategorieFragment.newInstance())
+                .commit();
     }
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
 
-        //onSectionAttached(position);
         FragmentManager fragmentManager = getSupportFragmentManager();
 
         switch (position)
         {
             case 0:
                 fragmentManager.beginTransaction()
-                        .replace(R.id.container, LeMieDomande.newInstance())
+                        .replace(R.id.container, LeMieDomandeFragment.newInstance())
                         .commit();
                 break;
 
             case 1:
                 fragmentManager.beginTransaction()
-                        .replace(R.id.container, PlaceholderFragment.newInstance(position))
+                        .replace(R.id.container, SceltaCategorieFragment.newInstance())
                         .commit();
                 break;
 
             case 2:
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, LoginFragment.newInstance())
+                        .commit();
                 break;
 
             case 3:
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, AboutFragment.newInstance())
+                        .commit();
                 break;
         }
-
-
     }
 
     public void onSectionAttached(int number) {
-        Log.d("SUGGESTME_onSectionAttached", ((Integer) number).toString());
         switch (number) {
             case 0:
                 mTitle = getString(R.string.ham_lemiedomande);
@@ -115,12 +117,8 @@ public class SceltaCategorie extends ActionBarActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -130,46 +128,59 @@ public class SceltaCategorie extends ActionBarActivity
 
     @Override
     public void onFragmentInteraction(Uri uri) {
-        return;
+
     }
 
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+    public static class SceltaCategorieFragment extends Fragment {
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
+        public static SceltaCategorieFragment newInstance() {
+
+            return new SceltaCategorieFragment();
         }
 
-        public PlaceholderFragment() {
+        public SceltaCategorieFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_scelta_categorie, container, false);
+
+            ImageButton mSocialButton = (ImageButton) rootView.findViewById(R.id.social_imgbtn);
+            ImageButton mGoodsButton  = (ImageButton) rootView.findViewById(R.id.goods_imgbn);
+
+
+            mSocialButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+
+                    getFragmentManager().beginTransaction()
+                            .replace(R.id.container, ChatFragment.newInstance(ChatFragment.SOCIAL))
+                            .commit();
+                }
+            });
+
+            mGoodsButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    getFragmentManager().beginTransaction()
+                            .replace(R.id.container, ChatFragment.newInstance(ChatFragment.GOODS))
+                            .commit();
+                }
+            });
+
             return rootView;
         }
 
         @Override
         public void onAttach(Activity activity) {
             super.onAttach(activity);
-            ((SceltaCategorie) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
+            //((SceltaCategorie) activity).onSectionAttached(
+            //       getArguments().getInt(ARG_SECTION_NUMBER));
         }
     }
 
